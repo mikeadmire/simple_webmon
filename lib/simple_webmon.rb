d8318b7408a832d99f47461e3709b3dadd3f72fb
Mike Admire <mike@mikeadmire.com>
2013-10-28 07:32:24 -0500

    Implement http request and verify status code is 200 for good response.

--- lib/simple_webmon.rb
+++ lib/simple_webmon.rb
@@ -1,9 +1,16 @@
 require "simple_webmon/version"
+require 'net/http'
+require 'uri'
 
 module SimpleWebmon
   class Monitor
     def get(url)
-      'OK'
+      res = Net::HTTP.get_response(URI.parse(url))
+      if res.code == "200"
+	return 'OK'
+      else
+	return 'DOWN'
+      end
     end
   end
 end
--- spec/lib/simple_webmon_spec.rb
+++ spec/lib/simple_webmon_spec.rb
@@ -2,8 +2,25 @@ require 'spec_helper'
 require 'simple_webmon'
 
 describe SimpleWebmon do
+  before(:all) do
+    FakeWeb.allow_net_connect = false
+
+    FakeWeb.register_uri(:get, "http://good.example.com/",
+			 body: "Hello World!")
+
+    FakeWeb.register_uri(:get, "http://servererror.example.com/",
+			 :body => "Internal Server Error",
+			 :status => ["500", "Internal Server Error"])
+  end
+
   it "returns 'OK' when given a URL for a properly responding site" do
     monitor = SimpleWebmon::Monitor.new
-    expect(monitor.get("http://www.example.com")).to eq 'OK'
+    expect(monitor.get("http://good.example.com/")).to eq 'OK'
   end
+
+  it "returns 'DOWN' when given a URL that responds with an Internal Server Error" do
+    monitor = SimpleWebmon::Monitor.new
+    expect(monitor.get("http://servererror.example.com/")).to eq 'DOWN'
+  end
+
 end
